4452d009a7a8c614573cc5adf54407cd40eff753
Viacheslav Chuvalskyi <slavchuval@gmail.com>
2021-10-25 11:22:08 +0300

    setting build script

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from "./store";
-import './assets/icons/video.svg';
 
 Vue.config.productionTip = false
 
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/https://github.com/ViacheslavChuvalkiy/7eminar_test/'
+    : '/'
+}
